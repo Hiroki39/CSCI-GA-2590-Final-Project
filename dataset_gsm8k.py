1f8ced69d32cf3f61bdd7a92ea4b829457d2bf92
45628andy <andy.wei.cc@gmail.com>
2023-04-22 20:22:49 -0400

    Add dataset processing/analysis scripts

---
+++ dataset_gsm8k.py
@@ -0,0 +1,57 @@
+import re
+from utils import extract_mapping
+
+from datasets import load_dataset
+
+# Loading the dataset
+
+dataset = load_dataset("gsm8k", "main")
+
+# Function for extracting mapping and answer from quesitons
+
+def extract_mapping_ans(dataset):
+
+    train_mappings = []
+    train_new_questions = []
+    train_num_answers = []
+
+    failed = []
+
+    # Replacing numbers with letters
+
+    for i, d in enumerate(dataset):
+
+        q = d['question']
+        a = d['answer']
+        
+        new_question, mapping = extract_mapping(q)
+
+        if (len(mapping) == 0):
+            failed.append((i,q))
+
+        # Finding numbers in the final answer
+
+        ans = None
+        try:
+            ans = re.findall(r"#### (.*)", a)[0]
+            ans = re.sub(r',', '', ans)
+            ans = float(ans)
+        except:
+            ans = None
+
+        train_mappings.append(mapping)
+        train_new_questions.append(new_question)
+        train_num_answers.append(ans)
+
+    # Adding new columns to train
+
+    dataset = dataset.add_column("mapping", train_mappings)
+    dataset = dataset.add_column("new_question", train_new_questions)
+    dataset = dataset.add_column("num_answer", train_num_answers)
+
+    return dataset, failed
+
+dataset['train'], train_failed = extract_mapping_ans(dataset['train'])
+dataset['test'], test_failed = extract_mapping_ans(dataset['test'])
+
+#dataset.save_to_disk("Data/processed_gsm8k")
