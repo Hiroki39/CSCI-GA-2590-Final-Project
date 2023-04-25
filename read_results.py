2c83448c45b06bb8359a13978cf01c12e68bf2b1
45628andy <andy.wei.cc@gmail.com>
2023-04-25 00:29:10 -0400

    Verifier for multiarith

--- read_results.py
+++ read_results.py
@@ -1,21 +1,92 @@
 import pandas as pd
+import numpy as np
+import math
+from collections import OrderedDict
+
 
 import argparse
-import openai
-from utils import evaluate_openai
-from uuid import uuid4
-from dotenv import load_dotenv
 import os
-import csv
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--run', type=str, required=True, default='gptturbo')
+def evaluate_equations(equations, mapping):
+    
+    new_variables = OrderedDict()
+    ceil = lambda x: math.ceil(x)
+    floor = lambda x: math.floor(x)
+    try:
+        # Extract variables from mapping
+        for i in mapping:
+            value = mapping[i]
+            exec(i + ' = ' + str(value))
 
-    result = pd.read_json(parser.run, lines=True)
+        # Evaluate equation one by one
+        for equation in equations:
+            elements = equation.split('=')
+            name = elements[0].strip()
+            expression = elements[1].strip()
 
-    for i in range(0, len(result)):
-        print("\nsample ",i,"\n")
-        print(result.response[i])
-        print("mapping: ",result.mapping[i])
-        print("answer: ",result.answer[i])
+            value = eval(expression)
+
+            exec(name + ' = ' + str(value))
+            new_variables[name] = value
+
+    except Exception as e:
+        print(e)
+        return None
+    
+    ret = list(new_variables.items())
+    
+    return ret[-1][1]
+
+def calculate_answer(result, prompt):
+
+    answers = []
+    equation_column = []
+    count = 0
+    if(prompt == 'arithcot'):
+        for i in range(len(result)):
+            response = result.response[i]
+            mapping = result.mapping[i]
+
+            equations = []
+
+            # Extract eqautions from response
+            for line in response.split('\n'):
+                if('=' in line):
+                    equations.append(line)
+
+            answer = evaluate_equations(equations, mapping)
+            answers.append(answer)
+            equation_column.append(equations)
+            if(answer is None):
+                print("*****\n",i,response,equations)
+                count += 1
+
+    else:
+        pass
+
+    print("invalid count", count)
+    return answers, equation_column
+
+
+#if __name__ == '__main__':
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('--logname', type=str, required=True)
+    #parser.add_argument('--prompt', type=str, required=True)
+
+filename = "2023-04-24 20-28-24.238771.jsonl"
+
+result = pd.read_json("logs/"+filename, lines=True)
+
+result['response_answer'], result['equations'] = calculate_answer(result, "arithcot")
+
+result['answer'] = [i[0] for i in result['answer']]
+
+print("acc",np.mean(result['response_answer'] == result['answer']))
+
+result.to_csv("experiment_results/"+ filename + ".csv")
+
+#for i in range(0, len(result)):
+#    print("\nsample ",i,"\n")
+#    print(result.response[i])
+#    print("mapping: ",result.mapping[i])
+#    print("answer: ",result.answer[i])
