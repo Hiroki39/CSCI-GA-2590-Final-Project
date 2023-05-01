8c292ace983a0d78ad93aca1646d84a5848cdbe7
y2499wan <catherinewang0304@gmail.com>
2023-05-01 10:48:11 -0400

    comment and clean up

--- dataset_svamp.py
+++ dataset_svamp.py
@@ -3,13 +3,15 @@ import pandas as pd
 from datasets import concatenate_datasets, load_dataset, load_from_disk
 from datasets import Dataset
 
+# preprocess svamp s.t. it has similar format as multiarith
 data = json.load(open('data/raw_SVAMP.json'))
+# change column names, map 'Body' to 'question', etc.
 new_d = {'Body': 'question', 'Answer': 'answer', 'Equation': 'equation', 'Type': 'type'}
 for row in data:
     for k,v in new_d.items():
         for name in list(row):
-            # print(name)
             if name == 'Body' and k==name:
+                # map 'Question' and 'Body' to 'questions'
                 row[v] = row.pop(name)+row.pop('Question')
             elif name == 'Answer' and k == name:
                 row[v] = [row.pop(name)]
