5d564d050858e3f766410b78ba5eae19df0eca68
Hiroki39 <zhenghongyi2000@hotmail.com>
2023-05-06 14:54:48 -0400

    finalize

--- plot_performance.py
+++ plot_performance.py
@@ -9,54 +9,70 @@ data = np.load('data/test_performance.npz', allow_pickle=True)
 # load data into dataframe
 df = pd.DataFrame(data['gptturbo_multiarith'], columns=[1, 2, 4, 8], index=[
     "CoT", "VarCoT", "PyCot", "SymPy"])
+df2 = pd.DataFrame(data["textdavinci_multiarith"], columns=[1, 2, 4, 8], index=[
+    "CoT", "VarCoT", "SymPy"])
 
 # melt dataframe
 df = df.reset_index(names=['Prompting Method']).melt(id_vars=['Prompting Method'], value_vars=[
+    1, 2, 4, 8], var_name='Shots', value_name='Accuracy')
+df2 = df2.reset_index(names=['Prompting Method']).melt(id_vars=['Prompting Method'], value_vars=[
     1, 2, 4, 8], var_name='Shots', value_name='Accuracy')
 
 sns.set_style("whitegrid")
 
 df["Shots"] = df["Shots"].astype(str)
+df["Model"] = "gpt-3.5-turbo"
+df2["Shots"] = df2["Shots"].astype(str)
+df2["Model"] = "text-davinci-002"
 
-g = sns.catplot(data=df, x='Shots', y='Accuracy',
-                hue='Prompting Method', kind='bar', alpha=0.6, height=6, aspect=1.5)
+df = pd.concat([df, df2])
+
+sns.catplot(data=df, x='Shots', y='Accuracy', hue='Prompting Method', col='Model',
+            kind='bar', alpha=0.6, height=2.5, aspect=1.8, hue_order=["CoT", "VarCoT", "SymPy", "PyCot"], palette="dark")
 # g.set_xticks([1, 2, 3, 4])
 # g.set_xticklabels(['1', '2', '4', '8'])
-plt.ylim(90, 100)
-plt.title('GPT-Turbo Multi-Arithmetic Dataset Accuracy')
+plt.ylim(40, 100)
 
-plt.savefig('images/gptturbo_multiarith.png', bbox_inches='tight')
+plt.savefig('images/multiarith.png', bbox_inches='tight')
 
-df = pd.DataFrame(data['textdavinci_multiarith'], columns=[2, 4, 8], index=[
-    "CoT", "VarCoT", "SymPy"])
-
-# melt dataframe
-df = df.reset_index(names=['Prompting Method']).melt(id_vars=['Prompting Method'], value_vars=[
-    2, 4, 8], var_name='Shots', value_name='Accuracy')
-
-df["Shots"] = df["Shots"].astype(str)
-
-g = sns.catplot(data=df, x='Shots', y='Accuracy',
-                hue='Prompting Method', kind='bar', alpha=0.6, height=6, aspect=1.5)
-
-plt.ylim(50, 100)
-plt.title('TextDavinci Multi-Arithmetic Dataset Accuracy')
-
-plt.savefig('images/textdavinci_multiarith.png', bbox_inches='tight')
-
-df = pd.DataFrame(data['textdavinci_aquarat'], columns=[1, 2, 4], index=[
+# load data into dataframe
+df = pd.DataFrame(data['gptturbo_aquarat'], columns=[1, 2, 4], index=[
+    "CoT", "SymPy"])
+df2 = pd.DataFrame(data["textdavinci_aquarat"], columns=[1, 2, 4], index=[
     "CoT", "SymPy"])
 
 # melt dataframe
 df = df.reset_index(names=['Prompting Method']).melt(id_vars=['Prompting Method'], value_vars=[
     1, 2, 4], var_name='Shots', value_name='Accuracy')
+df2 = df2.reset_index(names=['Prompting Method']).melt(id_vars=['Prompting Method'], value_vars=[
+    1, 2, 4], var_name='Shots', value_name='Accuracy')
 
 df["Shots"] = df["Shots"].astype(str)
+df["Model"] = "gpt-3.5-turbo"
+df2["Shots"] = df2["Shots"].astype(str)
+df2["Model"] = "text-davinci-002"
 
-g = sns.catplot(data=df, x='Shots', y='Accuracy',
-                hue='Prompting Method', kind='bar', alpha=0.6, height=6, aspect=1.5)
+df = pd.concat([df, df2])
 
-plt.ylim(0, 40)
-plt.title('TextDavinci AQUA-RAT Dataset Accuracy')
+sns.catplot(data=df, x='Shots', y='Accuracy', hue='Prompting Method', col='Model',
+            kind='bar', alpha=0.6, height=2.5, aspect=1.8, hue_order=["CoT", "SymPy"], palette="dark")
 
-plt.savefig('images/textdavinci_aquarat.png', bbox_inches='tight')
+plt.savefig('images/aquarat.png', bbox_inches='tight')
+
+
+df = pd.DataFrame(data['gptturbo_svamp'], columns=[1, 2, 4, 8], index=[
+    "CoT", "VarCoT", "PyCot", "SymPy"])
+
+# melt dataframe
+df = df.reset_index(names=['Prompting Method']).melt(id_vars=['Prompting Method'], value_vars=[
+    1, 2, 4, 8], var_name='Shots', value_name='Accuracy')
+
+df["Shots"] = df["Shots"].astype(str)
+df["Model"] = "gpt-3.5-turbo"
+
+sns.catplot(data=df, x='Shots', y='Accuracy', hue='Prompting Method', col='Model',
+            kind='bar', alpha=0.6, height=3, aspect=1, hue_order=["CoT", "VarCoT", "SymPy", "PyCot"], palette="dark")
+
+plt.ylim(60, 90)
+
+plt.savefig('images/svamp.png', bbox_inches='tight')
